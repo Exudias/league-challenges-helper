dc3762e1b35a2786a6dcd7447dfd79be1465a83f
Exudias <editoo31@gmail.com>
2024-07-25 17:14:43 +0300

    Test 6

--- netlify/functions/fetchApiKey.js
+++ netlify/functions/fetchApiKey.js
@@ -1,6 +1,8 @@
 export const handler = async () => {
   const value = process.env.RIOT_API_KEY;
 
+  console.log(value);
+
   return {
     statusCode: 200,
     body: JSON.stringify({ message: value }),
